83ce1d099154cbcab0749b68177e17991a24af80
sintesisestrategica <sintesisestrategica@gmail.com>
2025-07-10 18:44:56 -0300

    Agrego sección de logros, enlaces y organizo estructura del sitio

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/proyectos")
+def proyectos():
+    return render_template("proyectos.html")
+
+@app.route("/cucha")
+def cucha():
+    return render_template("cucha.html")
+
+@app.route("/articulos")
+def articulos():
+    return render_template("articulos.html")
+
+@app.route("/sintesis")
+def sintesis():
+    return render_template("sintesis.html")
+
+@app.route("/sobre_mi")
+def sobre_mi():
+    return render_template("sobre_mi.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
