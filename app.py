2d1dc922091ab3bd1b0388792e9006d5cb99221e
sbonacci33 <sbonacci89@gmail.com>
2025-07-21 00:44:00 -0300

    Simplify routing and fix template conflict

--- app.py
+++ app.py
@@ -1,39 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
-# Rutas
+PAGES = {
+    'trabajos',
+    'cucha',
+    'articulos',
+    'sintesis',
+    'sobre_mi',
+    'contacto',
+    'cv'
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/trabajos')
-def trabajos():
-    return render_template('trabajos.html')
-
-@app.route('/cucha')
-def cucha():
-    return render_template('cucha.html')
-
-@app.route('/articulos')
-def articulos():
-    return render_template('articulos.html')
-
-@app.route('/sintesis')
-def sintesis():
-    return render_template('sintesis.html')
-
-@app.route('/sobre_mi')
-def sobre_mi():
-    return render_template('sobre_mi.html')
-
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
-
-@app.route('/cv')
-def cv():
-    return render_template('cv.html')
+@app.route('/<page>')
+def render_page(page: str):
+    if page in PAGES:
+        return render_template(f"{page}.html")
+    abort(404)
 
 # Context processor para usar `request.endpoint` en templates
 @app.context_processor
